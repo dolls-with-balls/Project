2f6515265496a5daf39138e0965d8eed2fc54b76
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-02-16 19:10:25 +0800

    landingPage

--- unread/src/App.js
+++ unread/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { Home } from "./components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
