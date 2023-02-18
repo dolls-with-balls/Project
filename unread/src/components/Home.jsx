6fe10bc7d70f24c489aac1c7f0ffe099fb17ddc8
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-02-18 17:59:43 +0800

    asf

--- unread/src/components/Home.jsx
+++ unread/src/components/Home.jsx
@@ -1,27 +1,30 @@
 import style from "../designs/Home.module.css";
 import home from "../assets/Home.png";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
     <div className={style.container}>
       {/* <Header/> */}
       <div className={style.innerContainer}>
-        <div className={style.imgContainer}>
-          <img src={home} className={style.img} alt="home" />
-        </div>
-        <div className={style.textContainer}>
-          <div className={style.text2}>
-            <div>Brand New</div>
+        <Link to="/news/1g3g5g35gy" style={{display : "flex" , textDecoration : "none"}}>
+          <div className={style.imgContainer}>
+            <img src={home} className={style.img} alt="home" />
           </div>
-          <div className={style.text}>
-            "The Apprentice" цуврал #10: Шилдэг таван оролцогчийн өрсөлдөөн
+          <div className={style.textContainer}>
+            <div className={style.text2}>
+              <div>Brand New</div>
+            </div>
+            <div className={style.text}>
+              "The Apprentice" цуврал #10: Шилдэг таван оролцогчийн өрсөлдөөн
+            </div>
+            <div className={style.publisher}>
+              <div>image</div>
+              <div>Name</div>
+              <div>date</div>
+            </div>
           </div>
-          <div className={style.publisher}>
-            <div>image</div>
-            <div>Name</div>
-            <div>date</div>
-          </div>
-        </div>
+        </Link>
       </div>
       {/* <Header/> */}
     </div>
