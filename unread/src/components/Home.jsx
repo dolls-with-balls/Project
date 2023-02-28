7d17cba3aab6298efa4b34c7512b6ade071d692b
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-02-28 16:54:27 +0800

    landing

--- unread/src/components/Home.jsx
+++ unread/src/components/Home.jsx
@@ -7,29 +7,29 @@ export const Home = () => {
     <div className={style.container}>
       {/* <Header/> */}
       <div className={style.innerContainer}>
-        <Link
+        {/* <Link
           to="/news/1g3g5g35gy"
           style={{ display: "flex", textDecoration: "none" }}
-        >
-          <div className={style.imgContainer}>
-            <img src={home} className={style.img} alt="home" />
+        > */}
+        <div className={style.imgContainer}>
+          <img src={home} className={style.img} alt="home" />
+        </div>
+        <div className={style.textContainer}>
+          <div className={style.text2}>
+            <div>Brand New</div>
           </div>
-          <div className={style.textContainer}>
-            <div className={style.text2}>
-              <div>Brand New</div>
-            </div>
-            <div className={style.text}>
-              "The Apprentice" цуврал #10: Шилдэг таван оролцогчийн өрсөлдөөн
-            </div>
-            <div className={style.publisher}>
-              <div>image</div>
-              <div>Name</div>
-              <div>date</div>
-            </div>
+          <div className={style.text}>
+            "The Apprentice" цуврал #10: Шилдэг таван оролцогчийн өрсөлдөөн
           </div>
-        </Link>
+          <div className={style.publisher}>
+            <div>image</div>
+            <div>Name</div>
+            <div>date</div>
+          </div>
+        </div>
+        {/* </Link> */}
       </div>
-      {/* <Header/> */}
+      {/* <Footer/> */}
     </div>
   );
 };
