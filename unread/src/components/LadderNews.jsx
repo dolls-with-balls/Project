b9bc5791012015cf591fadda8a58fb039fd7e39c
heisenberg <sda@example.com>
2023-03-04 12:33:05 +0800

    ladder news

---
+++ unread/src/components/LadderNews.jsx
@@ -0,0 +1,124 @@
+import styles from "../style/LadderNews.module.css"
+
+export const LadderMews=()=>{
+    return(
+        <div className={styles.container}>
+            <div className={styles.innerContainer}>
+
+                <div className={styles.bigPost}>
+
+                </div>
+
+                {/* 1 */}
+
+                <div className={styles.smallPostContainer}>
+
+                    <div className={styles.postR}>
+                        <div className={styles.postInfoContainer}>
+                            <div className={styles.postInfoR}>
+                                <div className={styles.infoText}>
+                                    <div className={styles.profileNameR}>Majigsuren Tsogtbadrakh</div>
+
+                                    <div className={styles.infoDateContainerR}>
+                                        <div>3 sarrin 3, 2023 </div>
+                                    </div>
+                                </div>
+
+                                <div className={styles.infoImage}>
+                                    <img className={styles.profileImage} src="https://unread.today/files/profile/91cd813b12beb8323a656b349556e879.jpg"/>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className={styles.smallPostR}>
+                            <div className={styles.smallPostDesc}>
+                                Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic
+                            </div>
+
+                            <div className={styles.smallPostImageContainer}>
+                                <img className={styles.smallPostImage} src="https://unread.today/files/969a1aed-77a4-45e9-b7b7-3ee2709aaf31/7622443e7692181070e9dba4d8570e28_square.png"/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                {/* 2 */}
+
+                <div className={styles.smallPostContainer}>
+                    <div className={styles.postL}>
+                        <div className={styles.postInfoContainer}>
+                            <div className={styles.postInfoL}>
+                                <div className={styles.infoImage}>
+                                    <img className={styles.profileImage} src="https://unread.today/files/profile/91cd813b12beb8323a656b349556e879.jpg"/>
+                                </div>
+
+                                <div className={styles.infoText}>
+                                    <div className={styles.profileNameL}>Majigsuren Tsogtbadrakh</div>
+
+                                    <div className={styles.infoDateContainerL}>
+                                        <div>3 sarrin 3, 2023 </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className={styles.smallPostL}>
+                            <div className={styles.smallPostDesc}>
+                                Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic
+                            </div>
+
+                            <div className={styles.smallPostImageContainer}>
+                                <img className={styles.smallPostImage} src="https://unread.today/files/969a1aed-77a4-45e9-b7b7-3ee2709aaf31/7622443e7692181070e9dba4d8570e28_square.png"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* 3 */}
+
+                <div className={styles.smallPostContainer}>
+
+                    <div className={styles.postR}>
+                        <div className={styles.postInfoContainer}>
+                            <div className={styles.postInfoR}>
+                                <div className={styles.infoText}>
+                                    <div className={styles.profileNameR}>Majigsuren Tsogtbadrakh</div>
+
+                                    <div className={styles.infoDateContainerR}>
+                                        <div>3 sarrin 3, 2023 </div>
+                                    </div>
+                                </div>
+
+                                <div className={styles.infoImage}>
+                                    <img className={styles.profileImage} src="https://unread.today/files/profile/91cd813b12beb8323a656b349556e879.jpg"/>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className={styles.smallPostR}>
+                            <div className={styles.smallPostDesc}>
+                                Text messaging, or texting, is the act of composing and sending electronic messages, typically consisting of alphabetic
+                            </div>
+
+                            <div className={styles.smallPostImageContainer}>
+                                <img className={styles.smallPostImage} src="https://unread.today/files/969a1aed-77a4-45e9-b7b7-3ee2709aaf31/7622443e7692181070e9dba4d8570e28_square.png"/>
+                            </div>
+                        </div>
+                </div>
+
+
+                {/* 4 */}
+                
+
+
+            </div>
+
+            </div>
+        </div>
+    )
+}
