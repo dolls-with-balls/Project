1eb9b2d7e25bf924e852d6bc63ebb47651651d18
sanchirbilegt <cheesecake@LEAP-22s-iMac.local>
2023-03-04 18:20:30 +0800

    v

--- unread/src/components/Header.jsx
+++ unread/src/components/Header.jsx
@@ -10,41 +10,25 @@ import { BiSearch } from "react-icons/bi";
 import { FaLinkedinIn } from "react-icons/fa";
 export const Header = () => {
   return (
-    <>
-      <div className="header-container">
-        <div className="left-side-header">
-          <div className="logo-search">
-            <img className="logo" src={headerLogo} alt="" />
-            <p
-              style={{
-                fontSize: "20px",
-                margin: "0px",
-                color: "gainsboro"
-              }}
-            >
-              |
-            </p>
-            <div className="search">
-              <BiSearch className="search-icon" />
-              <input
-                className="search-bar"
-                type="text"
-                placeholder="Хайлт хийх"
-              />
-            </div>
+    <div className="header-container">
+      <div className="header-center">
+        <div className="header-nav">
+          <div className="header-search">
+            <a href="/">
+              <img className="logo" src={headerLogo} alt="" />
+            </a>
+            <span className="divider"></span>
+            <form className="header-form" action="/search">
+              <BiSearch className="search-icon"/>
+              <input className="search-bar" placeholder="Хайлт хийх" type="text" />
+            </form>
           </div>
-        </div>
-        <div className="right-side-header">
-          <div className="navbar-content">
-            <div className="nom61n-tuhai">
-              <p>Дараагийн нийтлэл:</p>
-              <h4>НОМЫН ТУХАЙ</h4>
-            </div>
-            <button>
-              Өнөөдөр орно <li className="dot" />
-            </button>
+          <div className="header-flip">
+            <span className="opacity05">Дараагийн нийтлэл:</span>
+            <span className="next-article-category">Номын тухай</span>
+            <button className="header-button">Өнөөдөр орно <li className="dot"></li></button>
           </div>
-          <div className="icon-container">
+          <div className="social-links">
             <AiFillFacebook className="icon" />
             <AiOutlineInstagram className="icon" />
             <AiOutlineTwitter className="icon" />
@@ -52,6 +36,6 @@ export const Header = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
