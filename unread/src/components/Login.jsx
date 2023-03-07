89745b4fec66b940603010285153140c5c4b998b
Monho12 <92088341+Monho12@users.noreply.github.com>
2023-03-07 18:50:01 +0800

    asd

--- unread/src/components/Login.jsx
+++ unread/src/components/Login.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import "../style/login.css"
+import "../style/login.css";
 
 export const Login = () => {
-    return (
-        <>
-            <div className="sign-up-container" >
-                <div className="left-side-background"></div>
-                <div className="email-pass-etc">
-                    <h2>Log in</h2>
-                    <input type="text" placeholder="Email" />
-                    <input type="text" placeholder="Password" />
-                    <button className="signup-button">
-                        Log in
-                    </button>
-                    <a href="/signup">Don't have an account?</a>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="sign-up-container">
+        <div className="left-side-background"></div>
+        <div className="email-pass-etc">
+          <h2>Log in</h2>
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Password" />
+          <button className="signup-button">Log in</button>
+          <a href="/signup">Don't have an account?</a>
+        </div>
+      </div>
+    </>
+  );
+};
