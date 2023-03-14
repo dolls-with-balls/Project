b7c86f840a93216e7b21fec100830cfd1d2788df
22hop2d-Munkhtsog <22hop2d-Munkhtsog@LEAP-10s-iMac.local>
2023-03-14 18:22:04 +0800

    profileBug

--- unread/src/App.js
+++ unread/src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Home } from "./pages/Home";
-import { Header } from "./components/Header";
+import { Signup } from "./pages/Signup";
 import { NewsDetail } from "./components/NewsDetail";
 import { UserProfile } from "./components/UserProfile";
-import { CarouseL } from "./components/Carousel";
 import { DataProvider } from "./contexts/DataProvider";
+import { CreateNews } from "./components/createNews"
 
 function App() {
   return (
@@ -16,9 +16,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/news" element={<NewsDetail />} />
-            <Route path="/carousel" element={<CarouseL />} />
-            <Route path="/userprofile" element={<UserProfile />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/news">
+              <Route path=":id" element={<NewsDetail />} />
+            </Route>
+            <Route path="/create" element={<CreateNews />} />
+            <Route path="/profile" element={<UserProfile />} />
           </Routes>
         </DataProvider>
       </BrowserRouter>
